3247ff5f1c0e79516246330d9b8b4f7042f58507
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-10-07 11:58:38 +0000

    scan bitcoin script

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::time::Duration;
 
 use bitcoincore_rpc::{Auth, Client, RpcApi};
 use dotenvy::dotenv;
+use bitcoin::Block;
 
 fn main() {
     dotenv().ok();
@@ -44,13 +45,33 @@ fn main() {
                 while current_height <= tip {
                     match client.get_block_hash(current_height) {
                         Ok(hash) => {
-                            match client.get_block_header_info(&hash) {
-                                Ok(info) => {
-                                    println!("{} {}", hash, info.n_tx);
+                            match client.get_block(&hash) {
+                                Ok(block) => {
+                                    let block: Block = block;
+                                    println!("{} {}", hash, block.txdata.len());
+                                    for tx in &block.txdata {
+                                        let txid = tx.compute_txid();
+                                        println!("tx {}:", txid);
+                                        for (i, input) in tx.input.iter().enumerate() {
+                                            let script_sig_hex = hex::encode(input.script_sig.as_bytes());
+                                            println!("  vin[{}] scriptSig: {}", i, script_sig_hex);
+                                            if let Some(wit) = (!input.witness.is_empty()).then(|| &input.witness) {
+                                                let mut wit_items: Vec<String> = Vec::new();
+                                                for w in wit.iter() {
+                                                    wit_items.push(hex::encode(w.as_ref()));
+                                                }
+                                                println!("  vin[{}] witness: [{}]", i, wit_items.join(", "));
+                                            }
+                                        }
+                                        for (j, output) in tx.output.iter().enumerate() {
+                                            let script_pubkey_hex = hex::encode(output.script_pubkey.as_bytes());
+                                            println!("  vout[{}] scriptPubKey: {}", j, script_pubkey_hex);
+                                        }
+                                    }
                                     current_height += 1;
                                 }
                                 Err(e) => {
-                                    eprintln!("error get_block_header_info at height {}: {}", current_height, e);
+                                    eprintln!("error get_block at height {}: {}", current_height, e);
                                     thread::sleep(Duration::from_millis(500));
                                 }
                             }
