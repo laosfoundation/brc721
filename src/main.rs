10d1e7d69b6b831432495ec6d33b61f2379af0cf
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-10-08 05:07:12 +0000

    refactoring and test

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,36 @@ use bitcoincore_rpc::{Auth, Client, RpcApi};
 use dotenvy::dotenv;
 use bitcoin::Block;
 
+fn format_block_scripts(block: &Block) -> String {
+    let mut out = String::new();
+    for tx in &block.txdata {
+        let txid = tx.compute_txid();
+        out.push_str(&format!("tx {}:\n", txid));
+        for (i, input) in tx.input.iter().enumerate() {
+            let script_sig_hex = hex::encode(input.script_sig.as_bytes());
+            out.push_str(&format!("  vin[{}] scriptSig: {}\n", i, script_sig_hex));
+            if !input.witness.is_empty() {
+                let wit_items: Vec<String> = input
+                    .witness
+                    .iter()
+                    .map(|w| hex::encode(w.as_ref()))
+                    .collect();
+                out.push_str(&format!("  vin[{}] witness: [{}]\n", i, wit_items.join(", ")));
+            }
+        }
+        for (j, output) in tx.output.iter().enumerate() {
+            let script_pubkey_hex = hex::encode(output.script_pubkey.as_bytes());
+            out.push_str(&format!("  vout[{}] scriptPubKey: {}\n", j, script_pubkey_hex));
+        }
+    }
+    out
+}
+
+fn process_block(block: &Block) {
+    let s = format_block_scripts(block);
+    print!("{}", s);
+}
+
 fn main() {
     dotenv().ok();
 
@@ -44,38 +74,17 @@ fn main() {
             Ok(tip) => {
                 while current_height <= tip {
                     match client.get_block_hash(current_height) {
-                        Ok(hash) => {
-                            match client.get_block(&hash) {
-                                Ok(block) => {
-                                    let block: Block = block;
-                                    println!("{} {}", hash, block.txdata.len());
-                                    for tx in &block.txdata {
-                                        let txid = tx.compute_txid();
-                                        println!("tx {}:", txid);
-                                        for (i, input) in tx.input.iter().enumerate() {
-                                            let script_sig_hex = hex::encode(input.script_sig.as_bytes());
-                                            println!("  vin[{}] scriptSig: {}", i, script_sig_hex);
-                                            if let Some(wit) = (!input.witness.is_empty()).then(|| &input.witness) {
-                                                let mut wit_items: Vec<String> = Vec::new();
-                                                for w in wit.iter() {
-                                                    wit_items.push(hex::encode(w.as_ref()));
-                                                }
-                                                println!("  vin[{}] witness: [{}]", i, wit_items.join(", "));
-                                            }
-                                        }
-                                        for (j, output) in tx.output.iter().enumerate() {
-                                            let script_pubkey_hex = hex::encode(output.script_pubkey.as_bytes());
-                                            println!("  vout[{}] scriptPubKey: {}", j, script_pubkey_hex);
-                                        }
-                                    }
-                                    current_height += 1;
-                                }
-                                Err(e) => {
-                                    eprintln!("error get_block at height {}: {}", current_height, e);
-                                    thread::sleep(Duration::from_millis(500));
-                                }
+                        Ok(hash) => match client.get_block(&hash) {
+                            Ok(block) => {
+                                println!("{} {}", hash, block.txdata.len());
+                                process_block(&block);
+                                current_height += 1;
                             }
-                        }
+                            Err(e) => {
+                                eprintln!("error get_block at height {}: {}", current_height, e);
+                                thread::sleep(Duration::from_millis(500));
+                            }
+                        },
                         Err(e) => {
                             eprintln!("error get_block_hash at height {}: {}", current_height, e);
                             thread::sleep(Duration::from_millis(500));
@@ -95,5 +104,51 @@ fn main() {
                 thread::sleep(Duration::from_secs(1));
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bitcoin::{Transaction, TxIn, TxOut, OutPoint};
+    use bitcoin::hashes::Hash;
+
+    #[test]
+    fn format_block_scripts_includes_scripts() {
+        let script_sig = bitcoin::ScriptBuf::from_bytes(vec![0x01, 0x02, 0x03]);
+        let script_pubkey = bitcoin::ScriptBuf::from_bytes(vec![0x51]);
+
+        let txin = TxIn {
+            previous_output: OutPoint::null(),
+            script_sig: script_sig.clone(),
+            sequence: bitcoin::Sequence::MAX,
+            witness: bitcoin::Witness::default(),
+        };
+        let txout = TxOut {
+            value: bitcoin::Amount::from_sat(0),
+            script_pubkey: script_pubkey.clone(),
+        };
+        let tx = Transaction {
+            version: bitcoin::transaction::Version::TWO,
+            lock_time: bitcoin::absolute::LockTime::ZERO,
+            input: vec![txin],
+            output: vec![txout],
+        };
+        let header = bitcoin::block::Header {
+            version: bitcoin::block::Version::TWO,
+            prev_blockhash: bitcoin::BlockHash::all_zeros(),
+            merkle_root: bitcoin::TxMerkleNode::all_zeros(),
+            time: 0,
+            bits: bitcoin::CompactTarget::from_consensus(0),
+            nonce: 0,
+        };
+        let block = Block {
+            header,
+            txdata: vec![tx],
+        };
+
+        let out = format_block_scripts(&block);
+        assert!(out.contains(&hex::encode(script_sig.as_bytes())));
+        assert!(out.contains(&hex::encode(script_pubkey.as_bytes())));
     }
 }
