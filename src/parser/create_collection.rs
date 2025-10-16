fe28ea1c73ab37d3aacab58df2a6bc14902c1c3d
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-10-16 08:47:43 +0000

    add tests

--- src/parser/create_collection.rs
+++ src/parser/create_collection.rs
@@ -1,51 +1,48 @@
 use bitcoin::ScriptBuf;
 
-pub fn digest(script: &ScriptBuf) {}
+use crate::types::RegisterCollectionPayload;
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use bitcoin::ScriptBuf;
-//     use std::str::FromStr;
-//
-//
-//     #[test]
-//     fn test_parse_register_collection_no_rebaseable() {
-//         let txout = bitcoin::TxOut {
-//             value: bitcoin::Amount::from_sat(0),
-//             script_pubkey: ScriptBuf::from_hex(
-//                 "6a5f0014ffff0123ffffffffffffffffffffffff3210ffff00",
-//             )
-//             .unwrap(),
-//         };
-//
-//         let r = parse_register_output0(&txout);
-//         assert!(r.is_some());
-//         let register_collection = r.unwrap();
-//         assert_eq!(
-//             register_collection.collection_address,
-//             CollectionAddress::from_str("ffff0123ffffffffffffffffffffffff3210ffff").unwrap()
-//         );
-//         assert!(!register_collection.rebaseable)
-//     }
-//
-//     #[test]
-//     fn test_parse_register_collection_rebaseable() {
-//         let txout = bitcoin::TxOut {
-//             value: bitcoin::Amount::from_sat(0),
-//             script_pubkey: ScriptBuf::from_hex(
-//                 "6a5f0014ffff0123ffffffffffffffffffffffff3210ffff01",
-//             )
-//             .unwrap(),
-//         };
-//
-//         let r = parse_register_output0(&txout);
-//         assert!(r.is_some());
-//         let register_collection = r.unwrap();
-//         assert_eq!(
-//             register_collection.collection_address,
-//             CollectionAddress::from_str("ffff0123ffffffffffffffffffffffff3210ffff").unwrap()
-//         );
-//         assert!(register_collection.rebaseable)
-//     }
-// }
+pub fn digest(script: &ScriptBuf) {
+    parse(script);
+}
+
+fn parse(script: &ScriptBuf) -> Option<RegisterCollectionPayload> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::types::CollectionAddress;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_parse_register_collection_no_rebaseable() {
+        let script =
+            ScriptBuf::from_hex("6a5f0014ffff0123ffffffffffffffffffffffff3210ffff00").unwrap();
+
+        let r = parse(&script);
+        assert!(r.is_some());
+        let register_collection = r.unwrap();
+        assert_eq!(
+            register_collection.collection_address,
+            CollectionAddress::from_str("ffff0123ffffffffffffffffffffffff3210ffff").unwrap()
+        );
+        assert!(!register_collection.rebaseable)
+    }
+
+    #[test]
+    fn test_parse_register_collection_rebaseable() {
+        let script =
+            ScriptBuf::from_hex("6a5f0014ffff0123ffffffffffffffffffffffff3210ffff01").unwrap();
+
+        let r = parse(&script);
+        assert!(r.is_some());
+        let register_collection = r.unwrap();
+        assert_eq!(
+            register_collection.collection_address,
+            CollectionAddress::from_str("ffff0123ffffffffffffffffffffffff3210ffff").unwrap()
+        );
+        assert!(register_collection.rebaseable)
+    }
+}
