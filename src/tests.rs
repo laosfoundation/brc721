14d31db1a0791dc74ada21fab2bb3ef64e9e5d47
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-10-26 17:38:09 +0000

    using local wallet

--- src/tests.rs
+++ src/tests.rs
@@ -1,16 +1,39 @@
 #[cfg(test)]
 mod tests {
-    use crate::wallet::Wallet;
-    use bdk_wallet::KeychainKind;
-    use bitcoin::{Amount, Network};
-
+    use bdk_wallet::{
+        bitcoin::{secp256k1::SecretKey, Network, PrivateKey},
+        template::P2Wpkh,
+        KeychainKind, Wallet,
+    };
     use bitcoincore_rpc::{Auth, Client, RpcApi};
-    use corepc_node::{Conf, Node};
-    use tempfile::Builder;
+    use corepc_node::Node;
+
+    #[test]
+    fn bdk_core_balance_compact() {
+        let node = Node::from_downloaded().unwrap();
+        let auth = Auth::CookieFile(node.params.cookie_file.clone());
+        let core = Client::new(&node.rpc_url(), auth.clone()).unwrap();
+
+        let network = Network::Regtest;
+        let k_ext = PrivateKey::new(SecretKey::new(&mut rand::thread_rng()), network);
+        let k_int = PrivateKey::new(SecretKey::new(&mut rand::thread_rng()), network);
+
+        let mut wallet = Wallet::create(P2Wpkh(k_ext), P2Wpkh(k_int))
+            .network(network)
+            .create_wallet_no_persist()
+            .expect("wallet");
+
+        let addr_info = wallet.reveal_next_address(KeychainKind::External);
+        core.generate_to_address(101, &addr_info.address)
+            .expect("mint");
+
+        let balance = wallet.balance();
+        assert_eq!(balance.total().to_btc(), 1.0);
+    }
 
     #[test]
     fn test_regtest_mine_and_check_balance() {
-        let node = corepc_node::Node::from_downloaded().expect("failed to download node");
+        let node = Node::from_downloaded().expect("failed to download node");
         let auth = Auth::CookieFile(node.params.cookie_file.clone());
 
         let client = Client::new(&node.rpc_url(), auth.clone()).expect("rpc client initial");
