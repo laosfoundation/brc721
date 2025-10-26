d20033e73200af832b2e3e0ff8538f20e6d3f3c2
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-10-26 13:06:56 +0000

    refactoring

--- src/tests.rs
+++ src/tests.rs
@@ -10,24 +10,25 @@ mod tests {
 
     #[test]
     fn test_regtest_mine_and_check_balance() {
-        let node = corepc_node::Node::from_downloaded().unwrap();
+        let node = corepc_node::Node::from_downloaded().expect("failed to download node");
         let auth = Auth::CookieFile(node.params.cookie_file.clone());
 
-        let client = Client::new(&node.rpc_url(), auth.clone()).expect("rpc client");
+        let client = Client::new(&node.rpc_url(), auth.clone()).expect("rpc client initial");
         let wallet_name = "test_wallet";
         client
             .create_wallet(wallet_name, None, None, None, None)
             .expect("wallet created and loaded");
 
         let rpc_url = format!("{}/wallet/{}", node.rpc_url(), wallet_name);
-        let client = Client::new(&rpc_url, auth).expect("rpc client");
+        let client = Client::new(&rpc_url, auth).expect("rpc client for wallet");
 
         let addr = client
             .get_new_address(None, None)
             .expect("new address")
             .assume_checked();
+
         client.generate_to_address(101, &addr).expect("mine");
-        let balances = client.get_balances().expect("balances");
+        let balances = client.get_balances().expect("get balances");
 
         assert_eq!(balances.mine.trusted.to_btc(), 50.0);
         assert_eq!(balances.mine.immature.to_btc(), 5000.0);
