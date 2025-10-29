29dff262d9973544be994d8f3b5fdb947cb2a78e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-10-29 09:57:24 +0000

    wallet integration test

---
+++ src/integration_tests/wallet.rs
@@ -0,0 +1,13 @@
+use bitcoin::Network;
+use tempfile::TempDir;
+
+use crate::wallet::Wallet;
+
+#[test]
+fn test_wallet_creation() {
+    let data_dir = TempDir::new().expect("temp dir");
+    let wallet = Wallet::new(data_dir.path(), Network::Regtest);
+
+    let ans = wallet.init(None, None).expect("wallet");
+    assert!(ans.created);
+}
