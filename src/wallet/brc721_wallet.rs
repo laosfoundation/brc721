ab3705d99641c16a01a15e1a2d0fb90a5572e506
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-10-31 09:17:14 +0000

    wallet takes network

--- src/wallet/brc721_wallet.rs
+++ src/wallet/brc721_wallet.rs
@@ -13,13 +13,12 @@ use crate::wallet::paths;
 struct Brc721Wallet;
 
 impl Brc721Wallet {
-    fn get_or_create<P: AsRef<Path>>(data_dir: P) -> Result<Brc721Wallet> {
+    fn get_or_create<P: AsRef<Path>>(data_dir: P, network: Network) -> Result<Brc721Wallet> {
         // Parse the deterministic 12-word BIP39 mnemonic seed phrase.
         let mnemonic = Mnemonic::parse_in(
             Language::English,
             "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
         ).unwrap();
-        let network = Network::Regtest;
 
         // Derive BIP32 master private key from seed.
         let seed = mnemonic.to_seed(String::new()); // empty password
@@ -45,10 +44,18 @@ mod tests {
     use tempfile::TempDir;
 
     #[test]
-    fn test_wallet_persist_on_storage() {
+    fn test_regtest_wallet_persist_on_storage() {
         let data_dir = TempDir::new().expect("temp dir");
-        Brc721Wallet::get_or_create(&data_dir).expect("wallet");
+        Brc721Wallet::get_or_create(&data_dir, Network::Regtest).expect("wallet");
         let expected_wallet_path = data_dir.path().join("wallet-regtest.sqlite");
+        assert!(expected_wallet_path.exists());
+    }
+
+    #[test]
+    fn test_bitcoin_wallet_persist_on_storage() {
+        let data_dir = TempDir::new().expect("temp dir");
+        Brc721Wallet::get_or_create(&data_dir, Network::Bitcoin).expect("wallet");
+        let expected_wallet_path = data_dir.path().join("wallet-mainnet.sqlite");
         assert!(expected_wallet_path.exists());
     }
 }
