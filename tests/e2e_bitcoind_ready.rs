0e17d79415d49686873ad7b1d6b6eb1328f3083e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-11-05 17:46:02 +0000

    removed one unuseful test

--- tests/e2e_bitcoind_ready.rs
+++ tests/e2e_bitcoind_ready.rs
@@ -5,47 +5,6 @@ mod e2e {
     use testcontainers::core::{ContainerPort, WaitFor};
     use testcontainers::runners::SyncRunner;
     use testcontainers::{GenericImage, ImageExt};
-
-    #[test]
-    fn bitcoind_rpc_is_ready() {
-        let image = GenericImage::new("bitcoin/bitcoin", "latest")
-            .with_wait_for(WaitFor::message_on_stdout("Binding RPC on address"))
-            .with_wait_for(WaitFor::message_on_stdout("init message: Done loading"))
-            .with_exposed_port(ContainerPort::Tcp(18443))
-            .with_cmd(vec![
-                "bitcoind".to_string(),
-                "-regtest=1".to_string(),
-                "-server=1".to_string(),
-                "-txindex=1".to_string(),
-                "-rpcbind=0.0.0.0".to_string(),
-                "-rpcallowip=0.0.0.0/0".to_string(),
-                "-rpcuser=dev".to_string(),
-                "-rpcpassword=dev".to_string(),
-            ]);
-
-        let container = image.start().expect("start bitcoind container");
-
-        let host_port = container
-            .get_host_port_ipv4(18443)
-            .expect("mapped port for 18443");
-
-        let rpc_url = format!("http://127.0.0.1:{}", host_port);
-        let auth = Auth::UserPass("dev".into(), "dev".into());
-
-        let client = Client::new(&rpc_url, auth).expect("rpc client");
-        let mut attempts = 0;
-        loop {
-            match client.get_block_count() {
-                Ok(_height) => break,
-                Err(_e) if attempts < 30 => {
-                    std::thread::sleep(Duration::from_secs(1));
-                    attempts += 1;
-                }
-                Err(e) => panic!("RPC not ready: {e}"),
-            }
-        }
-    }
-
     #[test]
     fn bitcoind_wallet_mine_and_balance() {
         let image = GenericImage::new("bitcoin/bitcoin", "latest")
