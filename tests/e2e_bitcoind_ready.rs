9e8883bceb2e9373b8f6daa67cc36d691fa597e3
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-11-06 06:36:20 +0000

    extracting helper function for image creation

--- tests/e2e_bitcoind_ready.rs
+++ tests/e2e_bitcoind_ready.rs
@@ -9,9 +9,9 @@ use std::time::Duration;
 use bitcoin::Address;
 use bitcoincore_rpc::{Auth, Client, RpcApi};
 use tempfile::TempDir;
-use testcontainers::core::{ContainerPort, WaitFor};
 use testcontainers::runners::SyncRunner;
-use testcontainers::{GenericImage, ImageExt};
+
+mod common;
 
 const MNEMONIC: &str =
     "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
@@ -40,23 +40,8 @@ fn base_cmd(rpc_url: &String, data_dir: &TempDir) -> ProcCommand {
 
 #[test]
 fn e2e_balance() {
-    let image = GenericImage::new("bitcoin/bitcoin", "latest")
-        .with_wait_for(WaitFor::message_on_stdout("Binding RPC on address"))
-        .with_wait_for(WaitFor::message_on_stdout("init message: Done loading"))
-        .with_exposed_port(ContainerPort::Tcp(18443))
-        .with_cmd(vec![
-            "bitcoind".to_string(),
-            "-regtest=1".to_string(),
-            "-server=1".to_string(),
-            "-txindex=1".to_string(),
-            "-rpcbind=0.0.0.0".to_string(),
-            "-rpcallowip=0.0.0.0/0".to_string(),
-            "-rpcuser=dev".to_string(),
-            "-rpcpassword=dev".to_string(),
-        ]);
-
+    let image = common::bitcoind_image();
     let container = image.start().expect("start bitcoind container");
-
     let host_port = container
         .get_host_port_ipv4(18443)
         .expect("mapped port for 18443");
@@ -65,18 +50,6 @@ fn e2e_balance() {
     let auth = Auth::UserPass("dev".into(), "dev".into());
 
     let root_client = Client::new(&rpc_url, auth.clone()).expect("rpc client initial");
-
-    let mut attempts = 0;
-    loop {
-        match root_client.get_block_count() {
-            Ok(_height) => break,
-            Err(_e) if attempts < 60 => {
-                std::thread::sleep(Duration::from_secs(1));
-                attempts += 1;
-            }
-            Err(e) => panic!("RPC not ready: {e}"),
-        }
-    }
 
     let data_dir = TempDir::new().expect("temp dir");
     let stdout = base_cmd(&rpc_url, &data_dir)
