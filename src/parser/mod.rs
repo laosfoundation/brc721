5a401977dcceeb47f858eea15a0132691d288cda
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-11-15 15:19:20 +0000

    log brc721 tx when found

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -52,6 +52,8 @@ impl Parser {
                 None => continue,
             };
 
+            log::info!("📦 Found BRC-721 tx at block {}, tx {}", block_height, tx_index);
+
             if let Some(Err(ref e)) = self.digest(brc721_tx, block_height, tx_index as u32) {
                 log::warn!("{:?}", e);
             }
