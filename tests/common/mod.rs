9177f9db8719122f4bfd183b09a9506583f98c0a
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-12-01 16:12:10 +0000

    fix the e2e tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -10,12 +10,13 @@ pub fn bitcoind_image() -> ContainerRequest<GenericImage> {
     GenericImage::new("bitcoin/bitcoin", "latest")
         .with_wait_for(WaitFor::message_on_stdout("Binding RPC on address"))
         .with_wait_for(WaitFor::message_on_stdout("init message: Done loading"))
-        .with_exposed_port(ContainerPort::Tcp(18443))
+        .with_mapped_port(18443, ContainerPort::Tcp(18443))
         .with_cmd(vec![
             "bitcoind".to_string(),
             "-regtest=1".to_string(),
             "-server=1".to_string(),
             "-txindex=1".to_string(),
+            "-printtoconsole=1".to_string(),
             "-rpcbind=0.0.0.0".to_string(),
             "-rpcallowip=0.0.0.0/0".to_string(),
             "-rpcuser=dev".to_string(),
